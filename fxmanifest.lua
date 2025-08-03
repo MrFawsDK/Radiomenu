194365bb8d6ca0ade0201b6f1f244b335ed991c5
Felix Sørensen <145162311+MrFawsDK@users.noreply.github.com>
2025-08-03 14:58:42 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Fawsdev'
+description 'Radio animationer'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_script 'client/main.lua'
+
+dependency 'ox_lib'
